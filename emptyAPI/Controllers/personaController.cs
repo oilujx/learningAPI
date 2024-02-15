2653777bf8a3ea97c08ceee9242d3a198c634480
Julio Luna <lunajulio1988@gmail.com>
2024-02-15 17:09:01 -0600

    Update personaController.cs

--- emptyAPI/Controllers/personaController.cs
+++ emptyAPI/Controllers/personaController.cs
@@ -1,37 +1,76 @@
-﻿using emptyAPI.Models;
+using emptyAPI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Runtime.InteropServices;
 using System.Web.Http;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace emptyAPI.Controllers
 {
     public class personaController : ApiController
     {
-        persona clPersona = new persona();
+        persona clPersona = null;
 
         [Route("api/Persona/"), HttpGet]
         public IHttpActionResult listarPersonas()
-        {
+        {            
             clPersona = new persona();
             return Ok(clPersona.listadoPersonas());
         }
 
-        [Route("api/Persona/"), HttpGet]
-        public IHttpActionResult listarPersonas(int dpi)
+        [Route("api/Persona/misNumeros"), HttpGet]
+        public IHttpActionResult misNumeros()
         {
             clPersona = new persona();
-            return Ok(clPersona.listarPersona(dpi));
+            return Ok(clPersona.misNumeros());
+        }       
+
+        [Route("api/Persona/"), HttpGet]
+        public IHttpActionResult listarPersonas(int dpi)
+        {            
+            clPersona = new persona();            
+            return Ok(clPersona.buscarPersona(dpi));
         }
 
         [Route("api/Persona/agregar"), HttpPost]
-        public IHttpActionResult crearPersona(persona _persona)
+        public IHttpActionResult crearPersona(List<persona> _personas)
         {
             clPersona = new persona();
-            return Ok(clPersona.agregarPersona(clPersona.listadoPersonas(), _persona));
+
+            List<persona> listaConPersonaNueva = new List<persona>();
+
+            // listaConPersonaNueva = clPersona.agregarPersona(_personas);
+
+            if (_personas.Count > 1)
+            {
+                listaConPersonaNueva = clPersona.agregarPersona(_personas);
+            }
+            else
+            {
+                persona newPerson = new persona();
+                newPerson = _personas[0];
+
+                listaConPersonaNueva = clPersona.agregarPersona(newPerson);
+            }
+            
+            return Ok(listaConPersonaNueva);
+        }
+
+        
+
+        [Route("api/Persona/json"), HttpPost]
+        public IHttpActionResult json(persona _persona)
+        {
+            clPersona = new persona();
+
+            string respuesta = clPersona.deserealizarObjeto(_persona);
+            Console.WriteLine(respuesta);
+
+            return Ok(clPersona.agregarPersona(_persona));
         }
     }
 }
